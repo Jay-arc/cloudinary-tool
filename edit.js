96cd1feb86b8050fce6448fddf1ac619954f06ab
Tyler Hasenoehrl <tyler.hasenoehrl@01614-MBP15S.local>
2018-03-06 09:07:15 -0800

    add edit.js

---
+++ edit.js
@@ -0,0 +1,31 @@
+// @ts-check
+$('#src_img').blur(function(){
+    console.log();
+    var src_img = $('#src_img').val();
+    $('#src_img_url').attr('src',src_img);
+});
+
+$('#unused_rules button').click(function(){
+    console.log('clicked');
+    var rule = $(this).text();
+    console.log(rule);
+    if(rule === "Flip Horizontal" ){
+        var existing_rules = $('#rules').val();
+        var new_rules = existing_rules + "/a_hflip";
+        $('#rules').val(new_rules);
+        add_rules(new_rules);
+    }
+    else if (rule === 'Flip Veritcal'){
+        var existing_rules = $('#rules').val();
+        var new_rules = existing_rules + "/a_vflip";
+        $('#rules').val(new_rules);
+        add_rules(new_rules);
+    }
+});
+
+function add_rules(new_rules){
+    var src_url = $('#src_img').val();
+    var split_url = src_url.split('/v');
+    var new_url = split_url[0]+new_rules+"/v"+split_url[1];
+    $('#complete_url').val(new_url);
+}
